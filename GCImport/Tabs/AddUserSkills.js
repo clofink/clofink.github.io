9e57c120f606e1f537d31d4db8223b2a669c1aec
Connor <connor.lofink@gmail.com>
2025-02-11 00:41:43 -0500

    Add the ability to add languages as well Major refactor

--- GCImport/Tabs/AddUserSkills.js
+++ GCImport/Tabs/AddUserSkills.js
@@ -3,7 +3,7 @@ class UserSkillsTab extends Tab {
 
     render() {
         window.requiredFields = ["Email", "Skills"];
-        window.allValidFields = ["Email", "Skills"];
+        window.allValidFields = ["Email", "Skills", "Languages"];
 
         this.container = newElement('div', { id: "userInputs" });
         const label = newElement('label', { innerText: "User Skills CSV: " });
@@ -17,60 +17,71 @@ class UserSkillsTab extends Tab {
         const logoutButton = newElement("button", { innerText: "Logout" });
         registerElement(logoutButton, "click", logout);
         const helpSection = addHelp([
-            `Must have "routing:readonly", "users" scope`,
-            `Required CSV column "Email", "Skills"`,
-            `"Skills" is a comma-separated list of skill names.`,
-            `Skill names are not case-sensitive`,
-            `Each skill can have a proficiency too. The format is <skillName>:<proficiency>`,
-            `If a skill is referenced that does not exist, it will be created`
+            `Must have "routing", "users" scope`,
+            `Required CSV column "Email"`,
+            `"Skills" and "Languages" are comma-separated lists of names.`,
+            `Skill and Language names are not case-sensitive`,
+            `Each skill or language can have a proficiency. The format is <name>:<proficiency>`,
+            `If a skill or language is referenced that does not exist, it will be created`
         ]);
         const exampleLink = createDownloadLink("User Skills Example.csv", Papa.unparse([window.allValidFields]), "text/csv");
         addElements([label, startButton, logoutButton, helpSection, exampleLink], this.container);
         return this.container;
     }
 
+    async getAllMapped(path, field = "name") {
+        const allItems = await getAllGenesysItems(path);
+        const map = {};
+        for (const item of allItems) {
+            map[item[field].toLowerCase()] = item.id;
+        }
+        return map;
+    }
+
+    async parseAndCreateIfNeeded(createPath, fullValue, fullMap, label) {
+        const items = [];
+        const names = [];
+        const itemValues = fullValue?.split(",") || [];
+        for (const itemValue of itemValues) {
+            const parts = itemValue.split(":");
+            const name = parts[0].toLowerCase().trim();
+            if (!fullMap[name]) {
+                const newItem = await makeCallAndHandleErrors(makeGenesysRequest, [createPath, "POST", { name: parts[0] }], results, parts[0], label);
+                fullMap[name] = newItem.id;
+            }
+            const value = parseInt(parts[1]);
+            if (parts[1] && (isNaN(value) || ![0, 1, 2, 3, 4, 5].includes(value))) {
+                results.push({ name: parts[0], type: "User Skill", status: "failed", error: `Invalid proficiency ${parts[1]} for ${label.toLowerCase()} ${parts[0]}` })
+                continue;
+            }
+            const item = {};
+            if (!parts[1]) item.proficiency = 0;
+            else item.proficiency = value;
+            item.id = fullMap[name];
+            items.push(item);
+            names.push(parts[0]);
+        }
+        return [items, names];
+    }
+
     async addUserSkills() {
         if (!fileContents) throw "No valid file selected";
 
-        const allSkills = await getAllGenesysItems("/api/v2/routing/skills?", 200, "entities");
-        const skillsInfo = {};
-        for (let skill of allSkills) {
-            skillsInfo[skill.name.toLowerCase()] = skill.id;
-        }
-        const allUsers = await getAllGenesysItems(`/api/v2/users?state=active`, 100, "entities");
-        const userInfo = {};
-        for (let user of allUsers) {
-            userInfo[user.email.toLowerCase()] = user.id;
-        }
+        const skillsInfo = await this.getAllMapped("/api/v2/routing/skills?");
+        const languagesInfo = await this.getAllMapped("/api/v2/routing/languages?");
+        const userInfo = await this.getAllMapped("/api/v2/users?state=active", "email");
 
         const results = [];
-        for (let user of fileContents.data) {
+        for (const user of fileContents.data) {
             if (!userInfo[user.Email.toLowerCase()]) {
                 results.push({ name: user.Email, type: "User Skill", status: "failed", error: `No active user matching email ${user.Email}` });
                 continue;
             }
-            const skills = [];
-            const values = user.Skills.split(",");
-            for (let value of values) {
-                const parts = value.split(":");
-                const skillName = parts[0].toLowerCase().trim();
-                if (!skillsInfo[skillName]) {
-                    const newSkill = await makeCallAndHandleErrors(makeGenesysRequest, ["/api/v2/routing/skills", "POST", { name: parts[0] }], results, parts[0], "Skill");
-                    skillsInfo[skillName] = newSkill.id;
-                }
-                const skillVal = parseInt(parts[1]);
-                if (parts[1] && (isNaN(skillVal) || ![0, 1, 2, 3, 4, 5].includes(skillVal))) {
-                    results.push({ name: parts[0], type: "User Skill", status: "failed", error: `Invalid proficiency ${parts[1]} for skill ${parts[0]}` })
-                    continue;
-                }
-                const skill = {};
-                if (!parts[1]) skill.proficiency = 0;
-                else skill.proficiency = skillVal;
-                skill.id = skillsInfo[skillName];
-                skills.push(skill);
-            }
-            if (skills.length < 1) continue;
-            await makeCallAndHandleErrors(makeGenesysRequest, [userInfo[user.Email.toLowerCase()], 'POST', skills], results, user.Email, "User Skills")
+            const [skills, skillNames] = await this.parseAndCreateIfNeeded("/api/v2/routing/skills/", user.Skills, skillsInfo, "Skill");
+            const [languages, languageNames] = await this.parseAndCreateIfNeeded("/api/v2/routing/languages/", user.Languages, languagesInfo, "Language");
+
+            if (skills.length > 0) await makeCallAndHandleErrors(makeGenesysRequest, [`/api/v2/users/${userInfo[user.Email.toLowerCase()]}/routingskills/bulk`, 'PATCH', skills], results, `${user.Email} assigned ${skillNames.join(", ")}`, "User Skills");
+            if (languages.length > 0) await makeCallAndHandleErrors(makeGenesysRequest, [`/api/v2/users/${userInfo[user.Email.toLowerCase()]}/routinglanguages/bulk`, 'PATCH', languages], results, `${user.Email} assigned ${languageNames.join(", ")}`, "User Skills");
         }
         return results;
     }
