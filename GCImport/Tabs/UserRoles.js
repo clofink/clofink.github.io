95dcc266f605f2a2f10b29ccb1e33f943a10ef77
Connor <connor.lofink@gmail.com>
2025-02-12 00:00:46 -0500

    Add export to user roles

--- GCImport/Tabs/UserRoles.js
+++ GCImport/Tabs/UserRoles.js
@@ -14,6 +14,10 @@ class UserRolesTab extends Tab {
         registerElement(startButton, "click", () => {
             showLoading(async () => { return this.bulkUpdateRoles() }, this.container);
         });
+        const exportButton = newElement('button', { innerText: "Export" });
+        registerElement(exportButton, "click", () => {
+            showLoading(async () => { return this.exportUserRoles() }, this.container);
+        })
         const logoutButton = newElement("button", { innerText: "Logout" });
         registerElement(logoutButton, "click", logout);
         const helpSection = addHelp([
@@ -25,7 +29,7 @@ class UserRolesTab extends Tab {
             `If no Division is included, "All Divisions" is used`
         ]);
         const exampleLink = createDownloadLink("Roles Example.csv", Papa.unparse([window.allValidFields]), "text/csv");
-        addElements([label, startButton, logoutButton, helpSection, exampleLink], this.container);
+        addElements([label, startButton, exportButton, logoutButton, helpSection, exampleLink], this.container);
         return this.container;
     }
 
@@ -33,26 +37,21 @@ class UserRolesTab extends Tab {
         return makeGenesysRequest(`/api/v2/authorization/subjects/${userId}/bulkadd`, "POST", roles);
     }
 
+    async getAllMapped(path, fieldFrom = "name", fieldTo = "id", normalize = true) {
+        const allItems = await getAllGenesysItems(path);
+        const map = {};
+        for (const item of allItems) {
+            map[normalize ? item[fieldFrom].toLowerCase() : item[fieldFrom]] = item[fieldTo];
+        }
+        return map;
+    }
+
     async bulkUpdateRoles() {
         if (!fileContents) throw "No valid file selected";
 
-        const allUsers = await getAllGenesysItems(`/api/v2/users?state=active`, 100, "entities");
-        const userInfo = {};
-        for (let user of allUsers) {
-            userInfo[user.email.toLowerCase()] = user.id;
-        }
-
-        const allRoles = await getAllGenesysItems("/api/v2/authorization/roles?", 200, "entities");
-        const rolesInfo = {};
-        for (let role of allRoles) {
-            rolesInfo[role.name.toLowerCase()] = role.id;
-        }
-
-        const allDivisions = await getAllGenesysItems("/api/v2/authorization/divisions?", 200, "entities");
-        const divisionInfo = {};
-        for (let division of allDivisions) {
-            divisionInfo[division.name.toLowerCase()] = division.id;
-        }
+        const userInfo = await this.getAllMapped(`/api/v2/users?state=active`, "email");
+        const rolesInfo = await this.getAllMapped("/api/v2/authorization/roles?");
+        const divisionInfo = await this.getAllMapped("/api/v2/authorization/divisions?");
 
         const results = [];
         for (let user of fileContents.data) {
@@ -86,5 +85,31 @@ class UserRolesTab extends Tab {
             await makeCallAndHandleErrors(this.updateRoles, [userInfo[user.Email.toLowerCase()], { grants: newRoles }], results, user.Email, "User Role");
         }
         return results;
+    }
+
+    async exportUserRoles() {
+        // this only accounts for 1 division for a role?
+        const users = await this.getAllMapped(`/api/v2/users?state=active`, "id", "email", false);
+        const roles = await this.getAllMapped("/api/v2/authorization/roles?", "id", "name", false);
+        const divisions = await this.getAllMapped("/api/v2/authorization/divisions?", "id", "name", false);
+        const userEmailToRolesMapping = {};
+        for (const [id, name] of Object.entries(roles)) {
+            const roleGrants = await getAllGenesysItems(`/api/v2/authorization/roles/${id}/subjectgrants?`);
+            for (const grant of roleGrants) {
+                if (!Object.hasOwnProperty.call(users, grant.id)) continue;
+                const userEmail = users[grant.id];
+                if (!Object.hasOwnProperty.call(userEmailToRolesMapping, userEmail)) userEmailToRolesMapping[userEmail] = [];
+                for (const division of grant.divisions) {
+                    if (division.id === "*") userEmailToRolesMapping[userEmail].push(name);
+                    else userEmailToRolesMapping[userEmail].push(`${name}:${divisions[division.id]}`);
+                }
+            }
+        }
+        const dataRows = [];
+        for (const [userEmail, rolesList] of Object.entries(userEmailToRolesMapping)) {
+            dataRows.push([userEmail, rolesList.join(",")]);
+        }
+        const downloadLink = createDownloadLink("User Roles Export.csv", Papa.unparse([["Email", "Roles"], ...dataRows]), "text/csv");
+        downloadLink.click();
     }
 }
